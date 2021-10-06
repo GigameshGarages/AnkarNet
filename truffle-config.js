c5df91893f92bf200a2e1afad7ddd2a00c9e1e69
Gokul Alex <gokulsan@users.noreply.github.com>
2021-10-06 11:23:34 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,37 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+require('babel-register')
+require('babel-polyfill')
+
+/**
+ * Use this file to configure your truffle project. It's seeded with some
+ * common settings for different networks and features like migrations,
+ * compilation and testing. Uncomment the ones you need or modify
+ * them to suit your project as necessary.
+ *
+ * More information about configuration can be found at:
+ *
+ * truffleframework.com/docs/advanced/configuration
+ *
+ * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
+ * to sign your transactions before they're sent to a remote public node. Infura accounts
+ * are available for free at: infura.io/register.
+ *
+ * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
+ * public/private key pairs. If you're publishing your code to GitHub make sure you load this
+ * phrase from a file you've .gitignored so it doesn't accidentally become public.
+ *
+ */
+
+const HDWalletProvider = require('@truffle/hdwallet-provider')
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+const MNEMONIC = process.env.MNEMONIC || 'clock radar mass judge dismiss just intact mind resemble fringe diary casino'
+const API_KEY = process.env.API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
   /**
@@ -34,73 +60,109 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
-
+  
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+    development: {
+      host: 'localhost',
+      port: 9545,
+      network_id: '*', // match any network
+      skipDryRun: true,
+      gas: 7000000
+    },
+    root: {
+      host: 'localhost',
+      port: 9545,
+      network_id: '*', // match any network
+      skipDryRun: true,
+      gas: 7000000,
+      gasPrice: '0'
+    },
+    child: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // match any network
+      skipDryRun: true,
+      gas: 7000000,
+      gasPrice: '0'
+    },
+    mumbaiRoot: {
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://goerli.infura.io/v3/${API_KEY}`
+        ),
+      network_id: 5,
+      gas: 7000000,
+      gasPrice: 10000000000, // 10 gwei
+      skipDryRun: true
+    },
+    mumbaiChild: {
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          'https://rpc-mumbai.matic.today'
+        ),
+      network_id: 80001,
+      gas: 7000000,
+      gasPrice: 10000000000, // 10 gwei
+      skipDryRun: true
+    },
+    mainnetRoot: {
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          `https://mainnet.infura.io/v3/${API_KEY}`
+        ),
+      network_id: 1,
+      gas: 7000000,
+      gasPrice: 10000000000, // 10 gwei
+      skipDryRun: true
+    },
+    mainnetChild: {
+      provider: () =>
+        new HDWalletProvider(
+          MNEMONIC,
+          'https://rpc-mainnet.matic.network'
+        ),
+      network_id: 137,
+      gas: 7000000,
+      gasPrice: 10000000000, // 10 gwei
+      skipDryRun: true
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      currency: 'USD',
+      gasPrice: 21,
+      outputFile: '/dev/null',
+      showTimeSpent: true
+    }
   },
 
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      version: '0.8.0', // Fetch exact version from solc-bin (default: truffle's version)
+      docker: true, // Use "0.5.1" you've installed locally with docker (default: false)
+      parser: 'solcjs',
+      settings: { // See the solidity docs for advice about optimization and evmVersion
+        // optimizer: {
+        //   enabled: false,
+        //   runs: 200
+        // }
+        evmVersion: 'istanbul'
+      }
     }
   },
 
-  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
-  //
-  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
-  // those previously migrated contracts available in the .db directory, you will need to run the following:
-  // $ truffle migrate --reset --compile-all
+  verify: {
+    preamble: 'Matic PoS Portal'
+  },
 
-  db: {
-    enabled: false
+  api_keys: {
+    etherscan: ETHERSCAN_API_KEY
   }
-};
+}
